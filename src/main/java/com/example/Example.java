b9d227aa3140d78f5aa454000eb4debd125b9dd5
danik <dan_ntu@mail.ru>
2019-03-18 10:42:23 +0600

    20.2.4 Disabling Restart

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Example {
     public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled","false");
         SpringApplication.run(Example.class, args);
     }
 
